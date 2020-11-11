788e57643431200de7757a96a15bf3d1e0a3decd
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2020-11-11 18:52:21 +0530

    setup event listeners to close dropdown when clicked outside or hit escape

--- src/components/CartDropdown/CartDropdown.js
+++ src/components/CartDropdown/CartDropdown.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useCallback } from 'react';
 import { withRouter } from 'react-router-dom';
 
 import CartItem from '../CartItem/CartItem';
@@ -11,8 +11,37 @@ import {
 } from './CartDropdown.styles';
 
 const CartDropdown = ({ cartItems, toggleCartHidden, history }) => {
+  const ref = useRef(null);
+
+  const clickListener = useCallback(
+    (e) => {
+      if (!ref.current.contains(e.target)) {
+        toggleCartHidden();
+      }
+    },
+    [toggleCartHidden]
+  );
+
+  const escapeListener = useCallback(
+    (e) => {
+      if (e.key === 'Escape') {
+        toggleCartHidden();
+      }
+    },
+    [toggleCartHidden]
+  );
+
+  useEffect(() => {
+    document.addEventListener('click', clickListener);
+    document.addEventListener('keyup', escapeListener);
+    return () => {
+      document.removeEventListener('click', clickListener);
+      document.removeEventListener('keyup', escapeListener);
+    };
+  }, [clickListener, escapeListener]);
+
   return (
-    <CartDropdownContainer>
+    <CartDropdownContainer ref={ref}>
       <CartItemsContainer>
         {cartItems.map((cartItem) => (
           <CartItem key={cartItem.id} item={cartItem} />
